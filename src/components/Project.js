00546abfb46003cfc2e7df7ce234472c7e9d78a2
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2024-07-01 17:23:53 +0530

    finishing up!

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,6 +16,11 @@ export const Projects = () => {
       imgUrl: projImg1,
     },
     {
+      title: "Hotel System",
+      description: "Machine Learning and development",
+      imgUrl: projImg2,
+    },
+    {
       title: "Human Emotion Detector",
       description: "Machine Learning and development",
       imgUrl: projImg2,
@@ -41,13 +46,7 @@ export const Projects = () => {
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="first">Try em out!</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
